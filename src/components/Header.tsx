42a662393ecfb8ffbbe07785870b4c2db3fba2b2
KoatKoetl <stas20027@outlook.com>
2025-06-24 16:42:24 +0300

    (Feature) - change the website name and logo to corresponding one's

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,9 +9,20 @@ import {
 const Header = () => {
   return (
     <>
-      <header className="flex items-center justify-between py-4">
+      <header className="flex items-center justify-between py-4 px-4 2xl:px-0">
         <NavigationMenu>
           <NavigationMenuList>
+            <NavigationMenuItem>
+              <NavigationMenuLink asChild>
+                <a className="w-full" href="/">
+                  <img
+                    className="max-w-10"
+                    src="/tabIcon.svg"
+                    alt="website logo"
+                  />
+                </a>
+              </NavigationMenuLink>
+            </NavigationMenuItem>
             <NavigationMenuItem className={`${navigationMenuTriggerStyle()}`}>
               <NavigationMenuLink
                 asChild
