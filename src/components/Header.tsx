d762962ee9c18cb05efde68f5991b1ab7fa4b7f5
Stanislav Suschevici <68298104+KoatKoetl@users.noreply.github.com>
2025-06-24 16:45:56 +0300

    Feature/create header (#2)
    * First commit
    
    * (Feature) - installed shadcn/UI library. Added navigation-menu component
    
    * (FIX) - ignore unimportant for now errors
    
    * (Feature) - add the UI menu in the header
    
    * (Feature) - change the website name and logo to corresponding one's

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,47 @@
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu";
+
 const Header = () => {
   return (
     <>
-      <header></header>
+      <header className="flex items-center justify-between py-4 px-4 2xl:px-0">
+        <NavigationMenu>
+          <NavigationMenuList>
+            <NavigationMenuItem>
+              <NavigationMenuLink asChild>
+                <a className="w-full" href="/">
+                  <img
+                    className="max-w-10"
+                    src="/tabIcon.svg"
+                    alt="website logo"
+                  />
+                </a>
+              </NavigationMenuLink>
+            </NavigationMenuItem>
+            <NavigationMenuItem className={`${navigationMenuTriggerStyle()}`}>
+              <NavigationMenuLink
+                asChild
+                className={`${navigationMenuTriggerStyle()}`}
+              >
+                <a href="/">Home</a>
+              </NavigationMenuLink>
+            </NavigationMenuItem>
+            <NavigationMenuItem className={`${navigationMenuTriggerStyle()}`}>
+              <NavigationMenuLink
+                asChild
+                className={`${navigationMenuTriggerStyle()}`}
+              >
+                <a href="/todo">ToDO</a>
+              </NavigationMenuLink>
+            </NavigationMenuItem>
+          </NavigationMenuList>
+        </NavigationMenu>
+      </header>
     </>
   );
 };
